60ae106209dfeeee2c97676df148c6c7350ebd7f
Ömer Acar <omeracar34@icloud.com>
2025-10-12 21:03:53 +0300

    initial commit

---
+++ src/main/java/com/omeracar/carventory/CarventoryApplication.java
@@ -0,0 +1,13 @@
+package com.omeracar.carventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omeracar/carventory/CarventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omeracar.carventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
